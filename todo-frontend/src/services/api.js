26350ec5b0d5c2818a36a58bcf2d2cc5a760b9e4
ragini377 <ragini.panwar377@gmail.com>
2026-02-28 18:57:02 +0530

    Update BASE_URL for API endpoint

--- todo-frontend/src/services/api.js
+++ todo-frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = `${import.meta.env.VITE_API_URL}/api/todos`;
 
-const BASE_URL = "https://todo-app-mern-r0pa.onrender.com";
+const BASE_URL = "https://todo-app-mern-sygt.onrender.com/api/todos";
 
 export const getTodos = () => axios.get(API_URL);
 export const createTodo = (data) => axios.post(API_URL, data);
