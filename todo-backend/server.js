a76a7e39e1c37c13dd71791f0ee967850d0d9d9e
ragini377 <ragini.panwar377@gmail.com>
2026-03-01 13:32:38 +0530

    Configure CORS with specific settings

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -9,6 +9,11 @@ connectDB();
 
 const app = express();
 app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 
